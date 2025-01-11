b50fcad3f4339f67b06f7290f3b68208562db861
ilguarnieri <guarnieriangelo91ag@gmail.com>
2025-01-11 11:04:14 +0100

    refactor js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,52 @@
-const burgerMenu = document.querySelector('.burger-menu');
+// Select DOM elements
+const body = document.body;
 const navMenu = document.querySelector('.menu-mobile');
-const body = document.querySelector('body');
+const burgerMenu = document.querySelector('.burger-menu');
 const menuItems = document.querySelectorAll('.menu-mobile li');
 
-
-burgerMenu.addEventListener('click', function(){
-  burgerMenu.classList.toggle('open');
-  navMenu.classList.toggle('show');
-  body.classList.toggle('overflow-hidden');
-})
-
-// Control Windows Width
-function checkWindowWidth() {
-  if (window.innerWidth > 1025) {
-    burgerMenu.classList.remove('open');
-    navMenu.classList.remove('menu-mobile', 'show');
-    navMenu.classList.add('menu-desktop');
-  }else {
-    navMenu.classList.add('menu-mobile');
-    navMenu.classList.remove('menu-desktop');
-  }
+// Handle window width changes
+function updateMenuLayout() {
+    if (window.innerWidth > 1025) {
+        resetMobileMenu();
+        navMenu.classList.replace('menu-mobile', 'menu-desktop');
+    } else {
+        navMenu.classList.replace('menu-desktop', 'menu-mobile');
+    }
 }
 
-checkWindowWidth();
+// Reset the mobile menu state
+function resetMobileMenu() {
+    burgerMenu.classList.remove('open');
+    navMenu.classList.remove('show');
+    body.classList.remove('overflow-hidden');
+}
 
-// Event listener Window Update
-window.addEventListener("resize", checkWindowWidth);
+// Toggle the mobile menu
+function toggleMobileMenu() {
+    burgerMenu.classList.toggle('open');
+    navMenu.classList.toggle('show');
+    body.classList.toggle('overflow-hidden');
+}
 
-// event listener menu items and add class active
-menuItems.forEach(item => {
-    item.addEventListener('click', function() {
-        menuItems.forEach(el => el.classList.remove('active'));
-        this.classList.add('active');
-        burgerMenu.click();
-    });
-});
+// Handle menu item activation
+function handleMenuItemClick() {
+    menuItems.forEach(item => item.classList.remove('active'));
+    this.classList.add('active');
+    toggleMobileMenu();
+}
+
+// Initialize event listeners
+function initEventListeners() {
+    burgerMenu.addEventListener('click', toggleMobileMenu);
+    window.addEventListener('resize', updateMenuLayout);
+    menuItems.forEach(item => item.addEventListener('click', handleMenuItemClick));
+}
+
+// Initialization
+function init() {
+    updateMenuLayout();
+    initEventListeners();
+}
+
+// Start the script
+init();
